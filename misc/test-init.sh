6445fbc8cda98e4005f5ebd91ed3ddd677f87929
lozinsky <jingle@live.ru>
2016-09-21 01:23:49 +0300

    Added babel-cli for build package, and new scripts

--- misc/test-init.sh
+++ misc/test-init.sh
@@ -1,7 +1,6 @@
 mkdir node_modules/typescript-babel-jest
-mkdir node_modules/typescript-babel-jest/source
-ln -s $(pwd)/source/preprocessor.js $(pwd)/node_modules/typescript-babel-jest/source
+
 ln -s $(pwd)/index.js $(pwd)/node_modules/typescript-babel-jest/
 
-ln -s $(pwd)/node_modules $(pwd)/tests/react/
-ln -s $(pwd)/node_modules $(pwd)/tests/common/
+ln -s $(pwd)/node_modules $(pwd)/examples/react/
+ln -s $(pwd)/node_modules $(pwd)/examples/common/
--- misc/test-rm.sh
+++ misc/test-rm.sh
@@ -1,3 +1,4 @@
 rm -rf $(pwd)/node_modules/typescript-babel-jest
-rm -rf $(pwd)/tests/common/node_modules
-rm -rf $(pwd)/tests/react/node_modules
+
+rm -rf $(pwd)/examples/common/node_modules
+rm -rf $(pwd)/examples/react/node_modules
